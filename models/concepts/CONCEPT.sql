78255449cad307bc7c039835647c67f338f1012e
Kwasi Opare Adu Labi <labiadu7@gmail.com>
2026-09-12 19:18:22 +0000

    fix:update Concept model

--- models/concepts/CONCEPT.sql
+++ models/concepts/CONCEPT.sql
@@ -1,22 +1,3 @@
-WITH raw_concept AS (
-    SELECT * FROM read_csv_auto(
-        'data/concepts/CONCEPT.csv',
-        delim = '\t',
-        header = true
-    )
-),
-raw_cvx AS (
-    SELECT * FROM read_csv_auto(
-        'data/concepts/CONCEPT_cvx.csv',
-        delim = '\t', 
-        header = true
-    )
-),
-combined AS (
-    SELECT * FROM raw_concept
-    UNION ALL
-    SELECT * FROM raw_cvx
-)
 SELECT
     CAST(concept_id AS INTEGER) AS concept_id,
     CAST(concept_name AS VARCHAR) AS concept_name,
@@ -28,4 +9,4 @@ SELECT
     CAST(strptime(CAST(valid_start_date AS VARCHAR), '%Y%m%d') AS DATE) AS valid_start_date,
     CAST(strptime(CAST(valid_end_date AS VARCHAR), '%Y%m%d') AS DATE) AS valid_end_date,
     CAST(invalid_reason AS VARCHAR) AS invalid_reason
-FROM combined
+FROM read_csv_auto('data/concepts/CONCEPT.csv', delim='\t', header=true)
